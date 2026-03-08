55a3958b917fd23d0549273edab61ae04fe816c4
Matheus <mateusbyakuya3@gmail.com>
2026-03-07 22:51:22 -0300

    feat: creating service client

--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateClientDto } from './dto/CreateClientDto';
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+  constructor(private prisma: PrismaService) { }
+
+  async create(userId: string, data: CreateClientDto) {
+    //eslint-disable-next-line
+    return await this.prisma.client.create({
+      data: {
+        ...data,
+        userId,
+      },
+    });
+  }
+
+  async findAll(userId: string) {
+    //eslint-disable-next-line
+    return await this.prisma.client.findMany({
+      where: { userId },
+    });
+  }
+}
