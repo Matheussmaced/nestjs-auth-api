301f463c248b97f03524f83b0db04775ce3e7208
Matheus <mateusbyakuya3@gmail.com>
2026-03-07 22:52:26 -0300

    feat: creating controller clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ClientsService } from './clients.service';
+import { CreateClientDto } from './dto/CreateClientDto';
 
-@Controller('clients')
-export class ClientsController {}
+@Controller('users/:userId/clients')
+export class ClientsController {
+  constructor(private readonly clientsService: ClientsService) { }
+
+  @Post()
+  create(@Param('userId') userId: string, @Body() data: CreateClientDto) {
+    return this.clientsService.create(userId, data);
+  }
+
+  @Get()
+  findAll(@Param('userId') userId: string) {
+    return this.clientsService.findAll(userId);
+  }
+}
