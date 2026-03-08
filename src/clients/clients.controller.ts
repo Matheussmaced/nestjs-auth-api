28c6e2fcffa7c22ed65c7c6c7c92c8016cf25a7e
Matheus <mateusbyakuya3@gmail.com>
2026-03-07 23:35:43 -0300

    feat: creating patch and delete for rout clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -5,10 +5,14 @@ import {
   Post,
   UseGuards,
   Request,
+  Param,
+  Patch,
+  Delete,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/CreateClientDto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UpdateClientDto } from './dto/UpdateClientDTO';
 
 @Controller('clients')
 export class ClientsController {
@@ -24,5 +28,20 @@ export class ClientsController {
   @Get()
   findAll(@Request() req) {
     return this.clientsService.findAll(req.user.userId);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Patch(':id')
+  update(
+    @Request() req,
+    @Param('id') id: string,
+    @Body() data: UpdateClientDto,
+  ) {
+    return this.clientsService.update(req.user.userId, id, data);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  delete(@Request() req, @Param('id') id: string) {
+    return this.clientsService.delete(req.user.userId, id);
   }
 }
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateClientDto } from './dto/CreateClientDto';
+import { UpdateClientDto } from './dto/UpdateClientDTO';
 
 @Injectable()
 export class ClientsService {
@@ -19,5 +20,42 @@ export class ClientsService {
     return this.prisma.client.findMany({
       where: { userId },
     });
+  }
+
+  async update(userId: string, clientId: string, data: UpdateClientDto) {
+    const client = await this.prisma.client.findFirst({
+      where: {
+        id: clientId,
+        userId: userId,
+      },
+    });
+
+    if (!client) {
+      throw new NotFoundException('Client not found');
+    }
+
+    return this.prisma.client.update({
+      where: { id: clientId },
+      data,
+    });
+  }
+
+  async delete(userId: string, clientId: string) {
+    const client = await this.prisma.client.findFirst({
+      where: {
+        id: clientId,
+        userId: userId,
+      },
+    });
+
+    if (!client) {
+      throw new NotFoundException('Client not found');
+    }
+
+    await this.prisma.client.delete({
+      where: { id: clientId },
+    });
+
+    return { message: 'Client deleted successfully' };
   }
 }
